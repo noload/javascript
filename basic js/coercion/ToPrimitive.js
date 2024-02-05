0f59b0965244245873f2692c0800d1c2b141189d
noload <vaibhavkamble023@gmail.com>
2024-02-05 22:19:59 +0530

    added toString and valueOf demo

--- basic js/coercion/ToPrimitive.js
+++ basic js/coercion/ToPrimitive.js
@@ -1,2 +1,31 @@
+/**ToPrimitive --
+ * This function takes an input  argument and convert it into non object .i.e primitive type
+ * if it cant convert it throws error
+ * It takes one more optional parameter known as prefered type
+ * prefered Type : - if we have  2  or more values that can become ans this opational args helps .
+ *
+ *
+ * If prefered type is not given , hint == default this is new variable we initilize
+ * else if prefered type is string, hint = "string"
+ *
+ * else prefered type is number, hint  = "number"
+ *
+ * if hint is default then we make hint to "number"
+ *
+ * At last it internally calls OrdinaryToPrimitive
+ *
+ */
 
-console.log();
+/**
+ * in OrdinaryToPrimitive abstract method
+ *
+ * if hint is String then we call toString()  and valueOf()
+ *
+ * if hint is "Number" then we call valueOf() and toString()
+ * 
+ * valueOf() and toString()are not abstrct operation thats  means we can call them
+ * 
+ * 
+ * By default toString() on object return  '[object Object]'
+ * By default valueOf on object returns same object
+*/
---
+++ basic js/coercion/valueOf_toString.js
@@ -0,0 +1,14 @@
+let obj = { name: "vaibhav", role: "Backend engineer" };
+
+console.log(obj.toString());
+console.log(obj.valueOf());
+
+
+let arr = [1, 2, 3];
+console.log(arr.valueOf());
+console.log(arr.toString());
+
+/**
+ * for array 
+ * toString return same array without bracket
+ */
